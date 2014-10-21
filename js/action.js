cc11c77296fc7b061127ee8ce41a367981dcf384
Tom Kennedy <tkennedy@smu.edu>
2014-10-21 09:53:01 -0500

    search button takes you to results

--- js/action.js
+++ js/action.js
@@ -1,3 +1,28 @@
+/*
+var allIngredients = getIngredients();
+
+function getIngredients(){
+
+	$.ajax({
+      type: 'GET',
+      url: rootURL+"/getIngredients",
+      dataType: "json", // data type of response
+      success: function(data, textStatus, jqXHR){
+         console.log(data);
+         return data;
+      },
+      error: function(jqXHR, textStatus, errorThrown){
+      	//alert("Burger not deleted");
+         console.log(jqXHR, textStatus, errorThrown);
+         window.location.reload();
+      }
+   });
+}
+*/
+console.log("in action");
+
+
+
 $('#addIngredient').click(function(){
 	var inputted = $('#textIngredient').val();
 	console.log(inputted);
@@ -6,4 +31,11 @@ $('#addIngredient').click(function(){
 	if(inputted != ""){
 		$('#ingredientList').append("<li class='ingredient'>"+ inputted + "</li>");
 	}
+});
+
+$("#search").click(function(){
+	alert("clicked");
+	console.log("go to results");
+	window.location.href = "results.html"
+
 });
