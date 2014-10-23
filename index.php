4d056d4d6647c47eac9ca81263023f5f5af53790
Brandon-Guerra <BrandonG481@gmail.com>
2014-10-23 10:44:04 -0500

    added dummy ingredient function

--- index.php
+++ index.php
@@ -9,6 +9,12 @@ ini_set('display_errors', 1);
 $app = new \Slim\Slim(); //using the slim API
 
 $app->get('/getIngredient', 'getIngredient'); //B public 
+$app->get('/dummyGetIngredient');
 $app->get('/getResult', 'getResult'); //end session and log out user 
 
+function dummyGetIngredient() {
+	$ingredients = ["egg", "butter", "chive"];
+	json_encode($ingredients);
+	echo $ingredients;
+}
 ?>
