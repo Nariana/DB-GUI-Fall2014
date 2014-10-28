67241958162f9d9eed1e9492605a84a3c0684550
Tom Kennedy <tkennedy@smu.edu>
2014-10-28 12:20:26 -0500

    framing recipe ajax

--- js/recipe.js
+++ js/recipe.js
@@ -1,1 +1,31 @@
 console.log(localStorage.getItem("selectedRecipe"));
+//XAMPP
+//var rootURL = "http://localhost/DB-GUI-Fall2014/index.php";
+//MAMP
+var rootURL = "http://localhost:8888/DB-GUI-Fall2014/api/index.php";
+
+
+load();
+
+function load(){
+
+	var recipeName = localStorage.getItem("selectedRecipe");
+
+	var send = {"recipeName": recipeName};
+
+	console.log(send);
+
+	$.ajax({
+        type: "GET",
+        url: rootURL+"/getRecipe",
+        dataType: "json",
+        data: send,
+        success: function (result) {
+            console.log(result);
+          },
+        error: function(jqXHR, textStatus, errorThrown){
+           console.log(jqXHR, textStatus, errorThrown);
+      }});
+
+
+}
